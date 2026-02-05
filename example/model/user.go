f7a4bc68c31dac5f66eba3b6c05833e728dfe827
Tetsuro Mikami <tetsuro@mickamy.com>
2026-02-05 19:11:06 +0900

    example: remove explicit db tags to demonstrate inference

--- example/model/user.go
+++ example/model/user.go
@@ -5,8 +5,8 @@ import "time"
 //go:generate go tool ormgen -source=$GOFILE -destination=../query
 
 type User struct {
-	ID        int       `db:"id,primaryKey"`
-	Name      string    `db:"name"`
-	Email     string    `db:"email"`
-	CreatedAt time.Time `db:"created_at"`
+	ID        int
+	Name      string
+	Email     string
+	CreatedAt time.Time
 }
